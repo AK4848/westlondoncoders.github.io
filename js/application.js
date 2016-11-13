708b02b7cd0a720dc6df5d0209a3eebedf41919f
Attila Keri <attila.keri@icloud.com>
2016-11-13 11:07:02 +0000

    added a new scrolling method to application.js

--- js/application.js
+++ js/application.js
@@ -5,3 +5,14 @@ $.fn.scrollView = function (selector) {
     }, 1000);
   });
 }
+$('a[href^="#"]').on('click',function (event) {
+	    event.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
